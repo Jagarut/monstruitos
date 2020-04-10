6efd1f70cd5aaa93224e4c205c0217763fdbc30b
Jag <jesusafonso.eam@gmail.com>
2020-04-10 18:24:47 +0100

    Updates

---
+++ precache-manifest.d4cf021afda5cda48023f81c1d4cf9c8.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "22e696a88ef0c5299035faf9b518dc6d",
+    "url": "/monstruitos/index.html"
+  },
+  {
+    "revision": "8d44d68fbd3b9362a11c",
+    "url": "/monstruitos/static/css/main.1c9d6e85.chunk.css"
+  },
+  {
+    "revision": "7e3553a52894256e16d6",
+    "url": "/monstruitos/static/js/2.aa8d843c.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/monstruitos/static/js/2.aa8d843c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "8d44d68fbd3b9362a11c",
+    "url": "/monstruitos/static/js/main.65aed1de.chunk.js"
+  },
+  {
+    "revision": "1e64c9b920ff4382b7ad",
+    "url": "/monstruitos/static/js/runtime-main.4b7b5e4d.js"
+  }
+]);
